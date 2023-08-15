62149f216e613a7d9d52b5b8502825f7141d5b38
Leo <45721247+Lemodo@users.noreply.github.com>
2023-08-15 19:21:13 +0200

    sign up functionality

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,17 +1,60 @@
-import React, { useRef } from 'react';
-import { Form, Button, Card } from "react-bootstrap";
+import React, { useRef, useState } from 'react';
+import { Form, Button, Card, Alert } from "react-bootstrap";
+import { useAuth } from '../contexts/AuthContext';
 
 export default function Signup() {
     const emailRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
+    const { signup } = useAuth()
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(false)
+
+
+    async function handleSubmit(e){
+        e.preventDefault()
+
+        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+            return setError('The passwords do not match')
+        }
+
+        try {
+            setError('')
+            setLoading(true)
+            await signup(emailRef.current.value, passwordRef.current.value)
+                .catch(error => {
+                    switch (error.code) {
+                        case 'auth/operation-not-allowed':
+                            setError('Account creation failed. Try again later. #0001')
+                            break;
+                        case 'auth/invalid-email':
+                            setError('The email is faulty.')
+                            break;
+                        case 'auth/invalid-password':
+                            setError('The password is faulty.')
+                            break;
+                        case 'auth/weak-password':
+                            setError('The password should be at least 6 characters. #0004')
+                            break;
+                        default:
+                            setError(error.message)
+                            break;
+                    }
+                })
+        } catch (e) {
+            setError(e)
+        }
+        setLoading(false)
+        
+    }
 
     return (
         <>
             <Card>
                 <Card.Body>
                     <h2 className='text-center mb-4'>Create your account</h2>
-                    <Form>
+                    {error && <Alert variant='danger'>{error}</Alert>}
+                    <Form onSubmit={handleSubmit}>
                         <Form.Group id="email">
                             <Form.Label>Email</Form.Label>
                             <Form.Control type='email' ref={emailRef} required></Form.Control>
@@ -24,7 +67,7 @@ export default function Signup() {
                             <Form.Label>Password Confirmation</Form.Label>
                             <Form.Control type='password' ref={passwordConfirmRef} required></Form.Control>
                         </Form.Group>
-                        <Button className="w-100 mt-3" type='submit'>Create account</Button>
+                        <Button className="w-100 mt-3" type='submit' disabled={loading}>Create account</Button>
                     </Form>
                 </Card.Body>
             </Card>
