f678ac2a7b82826427746831850924eb40f4c5b7
Leo <45721247+Lemodo@users.noreply.github.com>
2023-08-16 18:27:54 +0200

    tanstack react table

--- src/components/Database.js
+++ src/components/Database.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import Sidebar from './Sidebar'
-import BootstrapTable from 'react-bootstrap-table-next'
+import { useReactTable } from '@tanstack/react-table'
 
 export default function Database() {
+
+const table = useReactTable(data, columns)
+
 
   return (
     <div style={{ display: 'flex', height: '100%', minHeight: '100vh' }}>
       <Sidebar />
       <main style={{ padding: 10 }}> 
-      
+        
       </main>
     </div>
   )
