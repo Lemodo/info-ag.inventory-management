df63a679755e7abe1efa844887bee64f63956387
Leo <45721247+Lemodo@users.noreply.github.com>
2023-08-16 13:49:07 +0200

    sidebar

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <Sidebar>
+      <Menu>
+        <SubMenu label="Charts">
+          <MenuItem> Pie charts </MenuItem>
+          <MenuItem> Line charts </MenuItem>
+        </SubMenu>
+        <MenuItem> Documentation </MenuItem>
+        <MenuItem> Calendar </MenuItem>
+      </Menu>
+    </Sidebar>
   )
 }
