51689a3c365ba8717caf8cc2e015c42f5c8a2ae0
Leo <45721247+Lemodo@users.noreply.github.com>
2023-08-16 16:53:50 +0200

    mysql

--- src/components/Database.js
+++ src/components/Database.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Sidebar from './Sidebar'
+import BootstrapTable from 'react-bootstrap-table-next'
 
 export default function Database() {
+
   return (
     <div style={{ display: 'flex', height: '100%', minHeight: '100vh' }}>
       <Sidebar />
-      <main style={{ padding: 10 }}> Database view</main>
+      <main style={{ padding: 10 }}> 
+      
+      </main>
     </div>
   )
 }
